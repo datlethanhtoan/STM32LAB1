f8e91e5e6ecc04a3f97042204998a73ae61b6680
ASUS\Administrator <dat.lethanhtoan@hcmut.edu.vn>
2023-09-18 21:40:41 +0700

    update

--- bai1/Core/Src/main.c
+++ bai1/Core/Src/main.c
@@ -48,6 +48,8 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+void clearNumberOnClock(int);
+void setNumberOnClock(int);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -104,6 +106,96 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
+void setNumberOnClock(int num)
+{
+	 if (num >= 0 && num <= 11)
+	 {
+	switch (num)
+	        {
+	        case 0:
+	            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
+	            break;
+	        case 1:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+	        	break;
+	        case 2:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
+	            break;
+	        case 3:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+	            break;
+	        case 4:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
+	            break;
+	        case 5:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+	            break;
+	        case 6:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+	            break;
+	        case 7:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+	            break;
+	        case 8:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 0);
+	            break;
+	        case 9:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, 0);
+	            break;
+	        case 10:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, 0);
+	            break;
+	        case 11:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 0);
+	            break;
+	        }
+	 }
+}
+void clearNumberOnClock(int num)
+{
+if (num >= 0 && num <= 11)
+	 {
+	switch (num)
+	        {
+	        case 0:
+	            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 1);
+	            break;
+	        case 1:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+	        	break;
+	        case 2:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
+	            break;
+	        case 3:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
+	            break;
+	        case 4:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 1);
+	            break;
+	        case 5:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
+	            break;
+	        case 6:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 1);
+	            break;
+	        case 7:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 1);
+	            break;
+	        case 8:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 1);
+	            break;
+	        case 9:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, 1);
+	            break;
+	        case 10:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, 1);
+	            break;
+	        case 11:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 1);
+	            break;
+	        }
+	 }
+}
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
