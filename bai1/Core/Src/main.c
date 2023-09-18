6a5721255bd9c74d468a227169b57d4812971124
ASUS\Administrator <dat.lethanhtoan@hcmut.edu.vn>
2023-09-18 21:29:13 +0700

    update

--- bai1/Core/Src/main.c
+++ bai1/Core/Src/main.c
@@ -104,6 +104,51 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
+void setNumberOnClock(int num)
+{
+	 if (num >= 0 && num <= 11)
+	 {
+	switch (num)
+	        {
+	        case 0:
+	            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
+	            break;
+	        case 1:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+	        	break;
+	        case 2:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
+	            break;
+	        case 3:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+	            break;
+	        case 4:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
+	            break;
+	        case 5:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+	            break;
+	        case 6:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+	            break;
+	        case 7:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+	            break;
+	        case 8:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 0);
+	            break;
+	        case 9:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, 0);
+	            break;
+	        case 10:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, 0);
+	            break;
+	        case 11:
+	        	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 0);
+	            break;
+	        }
+	 }
+}
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
